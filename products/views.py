eca57d74d5cc0e7d343b6a40b61aabee10f294b2
Harish <attiharish1234@gmail.com>
2026-06-06 21:48:17 +0530

    update website fixes

--- products/views.py
+++ products/views.py
@@ -1,16 +1,33 @@
+from django.shortcuts import render, redirect
+from .models import Order
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def product(request):
+    return render(request, "product.html")
+
+
 def save_order(request):
     if request.method == "POST":
         Order.objects.create(
             product="Comfort Slippers",
             size=request.POST.get("size"),
             full_name=request.POST.get("full_name"),
-            address=request.POST.get("address") + ", " +
-                    request.POST.get("state") + " - " +
-                    request.POST.get("pincode"),
+            address=request.POST.get("address"),
             phone=request.POST.get("phone"),
             payment_status="Pending"
         )
-
         return redirect('/payment/')
 
     return redirect('/product/')
+
+
+def payment(request):
+    return render(request, "payment.html")
+
+
+def success(request):
+    return render(request, "success.html")
