f89622419014e8be403e9f3fe4e201b5abde484f
Harish <attiharish1234@gmail.com>
2026-06-07 12:16:37 +0530

    fixed payment page routing issues

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product/<int:id>/', views.product,
- name='product'),
-path('payment/', views.payment, name='payment'),
+    path('product/<int:id>/', views.product, name='product'),
+    path('save-order/', views.save_order, name='save_order'),
+    path('payment/', views.payment, name='payment'),
 ]
