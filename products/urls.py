d5b13b814e074d9d2a38a1a40ac451c6399f276b
Harish <attiharish1234@gmail.com>
2026-06-07 11:58:31 +0530

    fixed buy noe link and product page routing

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product/<int:id>/', views.product, name='product'),
+    path('product/<int:id>/', views.product,
+ name='product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# HOME PAGE - SHOW ALL PRODUCTS
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
 
 
-# PRODUCT DETAIL PAGE
 def product(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, "product.html", {'product': product})
